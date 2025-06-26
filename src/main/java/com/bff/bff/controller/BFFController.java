857f0394badfc1d4b9e9d7d1179a0948a05d8c96
Jacomazz <jacopo.mazzeo12@gmail.com>
2025-06-26 12:23:38 +0200

    Second Refactor

--- src/main/java/com/bff/bff/controller/BFFController.java
+++ src/main/java/com/bff/bff/controller/BFFController.java
@@ -1,8 +1,6 @@
 package com.bff.bff.controller;
 
-import com.bff.bff.BFFService.BFFServiceImpl;
 import com.bff.bff.models.catalog.BookDTO;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.client.RestTemplate;
@@ -15,20 +13,20 @@ public class BFFController {
 
 
     @Bean
-    public RestTemplate restTemplate() {
+    public RestTemplate getRestTemplate() {
         return new RestTemplate();
     }
 
-    @Autowired
-    private BFFServiceImpl serivce;
+//    @Autowired
+//    private BFFServiceImpl serivce;
 
     @GetMapping("")
-    public List<BookDTO> getBook() {
-        return null;
+    public List<Object> getBook() {
+        List<Object> bookList = (List<Object>) getRestTemplate().getForObject("localhost:8083/catalog",)
     }
 
     @PostMapping("")
-    public Object placeOrder(@RequestBody int idLibro, int nCopie, String codiceFiscale, String indirizzo) {
+    public Object placeOrder(@RequestBody BookDTO book) {
         return null;
     }
 
--- src/main/java/com/bff/bff/models/catalog/BookDTO.java
+++ src/main/java/com/bff/bff/models/catalog/BookDTO.java
@@ -1,17 +1,75 @@
 package com.bff.bff.models.catalog;
 
+import jakarta.validation.constraints.NotBlank;
 
-public class BookDTO extends BookDTOBase {
+public class BookDTO {
     
-    private int isbn;
+    @NotBlank(message="non mettere null o vuoto")
+    private String title;
+    private String genre;
+    private int publicationYear;
+    private String author;
+    private double price;
+    private int quantity = 0;
 
 
-    public int getIsbn() {
-        return this.isbn;
+
+
+    public String getTitle() {
+        return this.title;
     }
 
-    public void setIsbn(int isbn) {
-        this.isbn = isbn;
+    public void setTitle(String title) {
+        this.title = title;
     }
 
+    public String getGenre() {
+        return this.genre;
+    }
+
+    public void setGenre(String genre) {
+        this.genre = genre;
+    }
+
+    public int getPubblicationYear() {
+        return this.publicationYear;
+    }
+
+    public void setPubblicationYear(int pubblicationYear) {
+        this.publicationYear = pubblicationYear;
+    }
+
+    public String getAuthor() {
+        return this.author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public double getPrice() {
+        return this.price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public int getQuantity() {
+        return this.quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+
+    public BookDTO(String title, String genre, int publicationYear, String author, double price, int quantity) {
+        this.title = title;
+        this.genre = genre;
+        this.publicationYear = publicationYear;
+        this.author = author;
+        this.price = price;
+        this.quantity = quantity;
+    }
 }
